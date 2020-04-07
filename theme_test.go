fb7c68535c35da06ce74f012c23cb80bc57e674a
Jason Song <i@wolfogre.com>
2020-04-07 19:52:15 +0800

    test: more tests for theme

--- theme_test.go
+++ theme_test.go
@@ -1,6 +1,9 @@
 package emailt
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestAttributes_String(t *testing.T) {
 	tests := []struct {
@@ -32,6 +35,175 @@ func TestAttributes_String(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.as.String(); got != tt.want {
 				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMapTheme_Attributes(t *testing.T) {
+	type args struct {
+		tag string
+	}
+	tests := []struct {
+		name string
+		t    MapTheme
+		args args
+		want Attributes
+	}{
+		{
+			name: "regular",
+			t: MapTheme{
+				"a": Attributes{
+					{
+						Name:  "an",
+						Value: "av",
+					},
+				},
+			},
+			args: args{
+				tag: "a",
+			},
+			want: Attributes{
+				{
+					Name:  "an",
+					Value: "av",
+				},
+			},
+		},
+		{
+			name: "nil",
+			t:    nil,
+			args: args{
+				tag: "a",
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.t.Attributes(tt.args.tag); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Attributes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMapTheme_Exists(t *testing.T) {
+	type args struct {
+		tag string
+	}
+	tests := []struct {
+		name string
+		t    MapTheme
+		args args
+		want bool
+	}{
+		{
+			name: "regular",
+			t: MapTheme{
+				"a": Attributes{
+					{
+						Name:  "an",
+						Value: "av",
+					},
+				},
+			},
+			args: args{
+				tag: "a",
+			},
+			want: true,
+		},
+		{
+			name: "nil",
+			t:    nil,
+			args: args{
+				tag: "a",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.t.Exists(tt.args.tag); got != tt.want {
+				t.Errorf("Exists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChainTheme_Exists(t1 *testing.T) {
+	type fields struct {
+		upstream Theme
+		inner    Theme
+	}
+	type args struct {
+		tag string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "regular",
+			fields: fields{
+				upstream: MapTheme{
+					"b": Attributes{
+						{
+							Name:  "bn",
+							Value: "bv",
+						},
+					},
+				},
+				inner: MapTheme{
+					"a": Attributes{
+						{
+							Name:  "an",
+							Value: "av",
+						},
+					},
+				},
+			},
+			args: args{
+				tag: "a",
+			},
+			want: true,
+		},
+		{
+			name: "not exist",
+			fields: fields{
+				upstream: MapTheme{
+					"b": Attributes{
+						{
+							Name:  "bn",
+							Value: "bv",
+						},
+					},
+				},
+				inner: MapTheme{
+					"a": Attributes{
+						{
+							Name:  "an",
+							Value: "av",
+						},
+					},
+				},
+			},
+			args: args{
+				tag: "c",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			t := ChainTheme{
+				upstream: tt.fields.upstream,
+				inner:    tt.fields.inner,
+			}
+			if got := t.Exists(tt.args.tag); got != tt.want {
+				t1.Errorf("Exists() = %v, want %v", got, tt.want)
 			}
 		})
 	}
