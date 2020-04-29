c8945de8eebc1c63c2496c13eeaea48c0a76c956
Jason Song <i@wolfogre.com>
2020-04-29 18:54:20 +0800

    test: TestNewHeadline

--- headline_test.go
+++ headline_test.go
@@ -1,81 +1,53 @@
 package emailt
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"testing"
-
-	"golang.org/x/net/html"
 )
 
-func TestHeadline_Render(t *testing.T) {
-	type fields struct {
+func TestNewHeadline(t *testing.T) {
+	type args struct {
 		level  int
 		format string
 		a      []interface{}
 	}
-	type args struct {
-		themes []Theme
-	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
+		name string
+		args args
+		want Headline
 	}{
 		{
 			name: "regular",
-			fields: fields{
-				level:  1,
+			args: args{
+				level:  5,
 				format: "%v %v",
 				a:      []interface{}{1, "A"},
 			},
-			args:    args{},
-			wantErr: false,
+			want: "<h5>1 A</h5>",
 		},
 		{
-			name: "h-1",
-			fields: fields{
-				level:  -1,
+			name: "h0",
+			args: args{
+				level:  0,
 				format: "%v %v",
 				a:      []interface{}{1, "A"},
 			},
-			args:    args{},
-			wantErr: false,
+			want: "<h1>1 A</h1>",
 		},
 		{
 			name: "h7",
-			fields: fields{
+			args: args{
 				level:  7,
 				format: "%v %v",
 				a:      []interface{}{1, "A"},
 			},
-			args:    args{},
-			wantErr: false,
+			want: "<h6>1 A</h6>",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := NewHeadline(tt.fields.level, tt.fields.format, tt.fields.a...)
-
-			writer := &bytes.Buffer{}
-			err := h.Render(writer, tt.args.themes...)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Render() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			if got := NewHeadline(tt.args.level, tt.args.format, tt.args.a...); got != tt.want {
+				t.Errorf("NewHeadline() = %v, want %v", got, tt.want)
 			}
-
-			if _, err := html.Parse(bytes.NewReader(writer.Bytes())); err != nil {
-				t.Error(err)
-			}
-			t.Log(writer.String())
-
-			dir := "output"
-			_ = os.Mkdir(dir, 0744)
-			_ = ioutil.WriteFile(filepath.Join(dir, fmt.Sprintf("TestHeadline_Render.%s.html", tt.name)), writer.Bytes(), 0644)
 		})
 	}
 }
